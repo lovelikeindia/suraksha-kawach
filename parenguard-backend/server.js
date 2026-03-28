f60a5f8c477729f3cf7b39d874cea500b67d592c
lovelikeindia <lovelikeindia7@gmail.com>
2026-03-28 22:40:24 +0530

    Add files via upload

--- parenguard-backend/server.js
+++ parenguard-backend/server.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
